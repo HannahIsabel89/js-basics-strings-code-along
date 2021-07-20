a7adf26171a3b81e0ccab3e9de9d7d77fbcb312e
Hannah <hannahwoodward@hotmail.com>
2021-07-20 11:50:44 +0800

    messages correct

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+function welcomeMessage() {
+  console.log ('Welcome to Flatbook, ' + currentUser);
+}
+welcomeMessage();
+
+function excitedWelcomeMessage () {
+  console.log ('WELCOME TO FLATBOOK, ' + currentUser.toLocaleUpperCase() + '!');
+}
+excitedWelcomeMessage();
+
+const firstInitial = currentUser[0]
+function shortGreeting () {
+  console.log ('Welcome, ' + `${firstInitial} ${currentUser.slice(6)}!`);
+}
+shortGreeting();
